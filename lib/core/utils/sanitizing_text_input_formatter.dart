6d6ede1dfe67b309e0e58015ea5dd8f590a1caa3
Harsh Raj <harshuraj423@gmail.com>
2026-08-24 16:40:23 +0530

    Text Sanitization Updated

--- lib/core/utils/sanitizing_text_input_formatter.dart
+++ lib/core/utils/sanitizing_text_input_formatter.dart
@@ -9,8 +9,11 @@ class SanitizingTextInputFormatter extends TextInputFormatter {
   ) {
     // Strip out non-printable ASCII and invisible Unicode characters
     // such as RLO (Right-to-Left Override \u202E), zero-width spaces, etc.
-    final sanitizedText = newValue.text.replaceAll(RegExp(r'[\x00-\x1F\x7F-\x9F\u200B-\u200D\uFEFF\u202A-\u202E]'), '');
-    
+    final sanitizedText = newValue.text.replaceAll(
+      RegExp(r'[\x00-\x1F\x7F-\x9F\u200B-\u200D\uFEFF\u202A-\u202E]'),
+      '',
+    );
+
     if (sanitizedText == newValue.text) {
       return newValue;
     }
